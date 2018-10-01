1d1e66800ef3876c5644f6042b766dc1a0a0985a
dpwbailey <dpwbailey@gmail.com>
2018-10-01 11:54:13 -0400

    Updated OOP Project Alpha based on desired output from repl.it

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main extends Product implements Item {
+
+  public static void main(String[] args) {
+
+    Widget w1 = new Widget("Widget 1");
+    System.out.println(w1.toString());
+    Widget w2 = new Widget("Widget 2");
+    System.out.println(w2.toString());
+
+    for (ItemType it : ItemType.values()) {
+      System.out.println(it + " " + it.code);
+    }
+
+  }
+
+}
+
+
+
---
+++ src/Product.java
@@ -0,0 +1,65 @@
+/**
+ * Programmer: Daniel Bailey
+ * Description: OOP Project
+ * Date: 10/1/2018
+ **/
+
+
+import java.util.Date; //needed to get current time etc
+
+public abstract class Product implements Item {
+
+  private int serialNumber;
+  private String manufacturer = "OraclProduction";
+  private Date manufacturedOn = new Date();
+  private String name = "default name";
+  private static int currentProductionNumber = 1;
+  private ItemType code;
+
+  public void setProductionNumber(int num) {
+    currentProductionNumber = num;
+    serialNumber = currentProductionNumber;
+    currentProductionNumber++;
+  }
+
+  public void setName(String name) {
+
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getManufactureDate(String date) {
+    //    manufacturedOn.getTime();
+    return manufacturedOn.toString();
+  }
+
+  public int getSerialNumber(int serialNum) {
+    return serialNumber;
+  }
+
+  //prints info
+  public String toString() {
+    System.out.println("Manufacturer  : " + manufacturer);
+    System.out.println("Serial Number : " + serialNumber);
+    System.out.println("Date          : " + manufacturedOn);
+    System.out.println("Name          : " + name);
+    return "";
+  }
+
+  //default constructor
+  public Product() {
+  }
+
+  //constructor
+  public Product(String pName) {
+    setName(pName);
+    setProductionNumber(currentProductionNumber);
+    manufacturedOn.getTime();
+    toString();
+
+  }
+}
+
